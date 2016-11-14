3d66718e700bc9aab5b47bfd0c8c1dd7170ec560
mklf <golifang1234@gmail.com>
2016-11-14 09:21:41 +0800

    change dict

--- src/dictionary.rs
+++ src/dictionary.rs
@@ -6,13 +6,16 @@ use std::collections::HashMap;
 
 const MAX_VOCAB_SIZE: usize = 30000000;
 
-pub struct Dict {
-    word2idx: HashMap<String, Entry>,
-    idx2count: Vec<u32>,
+pub struct Dict<'a> {
+    word2ent: HashMap<String, Entry>,
+    idx2word: HashMap<usize, &'a str>,
     ntokens: usize,
     size: usize,
 }
-struct Entry(usize, u32);
+struct Entry {
+    index: usize,
+    count: u32,
+}
 
 pub fn hash(word: &str) -> usize {
     let mut h = 2166136261usize;
@@ -23,51 +26,66 @@ pub fn hash(word: &str) -> usize {
     h % MAX_VOCAB_SIZE
 }
 
-impl Dict {
-    pub fn new() -> Dict {
+impl<'a> Dict<'a> {
+    pub fn new() -> Dict<'a> {
         Dict {
-            word2idx: HashMap::with_capacity(100000),
-            idx2count: Vec::new(),
+            word2ent: HashMap::new(),
+            idx2word: HashMap::new(),
             ntokens: 0,
             size: 0,
         }
     }
+    #[inline]
+    fn add_to_dict(words: &mut HashMap<String, Entry>, word: &str, size: &mut usize) {
+        if !words.contains_key(word) {
+            words.insert(word.to_string(),
+                         Entry {
+                             index: *size,
+                             count: 1,
+                         });
 
+            *size += 1;
+        } else {
+            if let Some(x) = words.get_mut(word) {
+                x.count += 1;
+            }
+        }
+    }
 
-    pub fn read_from_file(&mut self, filename: &str) {
+    pub fn read_from_file(filename: &str) -> Dict {
         let mut input_file = File::open(filename).unwrap();
         let mut reader = BufReader::with_capacity(10000, input_file);
         let mut buf_str = String::with_capacity(5000);
-        let mut size = self.size;
-        let mut words = HashMap::new();
+        let mut words: HashMap<String, Entry> = HashMap::new();
+        let mut idx2word: HashMap<usize, &str> = HashMap::new();
+        let mut ntokens = 0;
+        let mut size = 0;
         while reader.read_line(&mut buf_str).unwrap() > 0 {
             for word in buf_str.split_whitespace() {
-                self.ntokens += 1;
-                if !words.contains_key(word) {
-                    words.insert(word.to_string(), Entry(size, 1));
-                    size += 1;
-                } else {
-                    if let Some(x) = words.get_mut(word) {
-                        x.1 += 1;
-                    }
-                }
-                if self.ntokens % 1000000 == 0 {
-                    print!("\r read {}M words", self.ntokens / 1000000);
+                Dict::add_to_dict(&mut words, word, &mut size);
+                if ntokens % 1000000 == 0 {
+                    print!("\r read {}M words", ntokens / 1000000);
                     io::stdout().flush().ok().expect("Could not flush stdout");
                 }
             }
             buf_str.clear();
         }
-        let mut s = String::new();
-        io::stdin().read_to_string(&mut s);
         size = 0;
-        self.word2idx = words.into_iter().filter(|&(_, ref v)| v.1 >= 5).collect();
-        for (_, v) in self.word2idx.iter_mut() {
-            v.0 = size;
+        let mut word2ent: HashMap<String, Entry> =
+            words.into_iter().filter(|&(_, ref v)| v.count >= 5).collect();
+        for (_, v) in word2ent.iter_mut() {
+            v.index = size;
             size += 1;
         }
-        println!("\r Read {} M words", self.ntokens / 1000000);
-        let mut s = String::new();
-        io::stdin().read_to_string(&mut s);
+        for (k, v) in &word2ent {
+            idx2word.insert(v.index, &k);
+        }
+        println!("\r Read {} M words", ntokens / 1000000);
+        Dict {
+            word2ent: word2ent,
+            idx2word: idx2word,
+            ntokens: ntokens,
+            size: size,
+        }
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,11 @@
 extern crate word2vec;
 use word2vec::dictionary::*;
+use std::io;
 fn main() {
     println!("{:?}", "hello world");
     let mut d = Dict::new();
-    
+
     d.read_from_file("../wiki_seg_5e4.txt");
+    let mut s = String::new();
+    std::io::stdin().read_line(&mut s);
 }
